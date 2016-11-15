244ab2639a6201327b19beb8ef743c6fac8c049d
Martin Kåberg <martin.kaberg@nordcloud.se>
2016-11-15 10:30:04 +0700

    more

--- create-on-aws-linux.sh
+++ create-on-aws-linux.sh
@@ -1,53 +1,87 @@
 #!/usr/bin/env bash
+
 set -eu
 KMS_KEY_ARN=$1
 function wait_for_stack()
 {
-  set -eu
-  declare _stack_name=$1
-  declare _status
-  while sleep 2
-  do
-    _status=$(aws cloudformation describe-stacks --stack-name ${_stack_name} | jq -r '.Stacks[].StackStatus')
-    if [[ ${_status} == "CREATE_COMPLETE" ]]
-    then
-      break
-    fi
-    if [[ ${_status} == "UPDATE_COMPLETE" ]]
-    then
-      break
-    fi
-    if [[ ${_status}  =~ .*FAILED.* ]]
-    then
-      return 1
-    fi
-    if [[ ${_status} =~ .*ROLLBACK.* ]]
-    then
-      return 1
-    fi
-    if [[ ${_status} =~ .*DELETE.* ]]
-    then
-      return 1
-    fi
-  done
+	set -eu
+	declare _stack_name=$1
+	declare _status
+	while sleep 2
+	do
+		_status=$(aws cloudformation describe-stacks --stack-name ${_stack_name} | jq -r '.Stacks[].StackStatus')
+		if [[ ${_status} == "CREATE_COMPLETE" ]]
+		then
+			break
+		fi
+		if [[ ${_status} == "UPDATE_COMPLETE" ]]
+		then
+			break
+		fi
+		if [[ ${_status} =~ .*FAILED.* ]]
+		then
+			return 1
+		fi
+		if [[ ${_status} =~ .*ROLLBACK.* ]]
+		then
+			return 1
+		fi
+		if [[ ${_status} =~ .*DELETE.* ]]
+		then
+			return 1
+		fi
+	done
 }
+
+function stack_exists()
+{
+	set -eu
+	declare _stack_name=$1
+	declare _filter=(
+		CREATE_IN_PROGRESS
+		CREATE_FAILED
+		CREATE_COMPLETE
+		ROLLBACK_IN_PROGRESS
+		ROLLBACK_FAILED
+		ROLLBACK_COMPLETE
+		DELETE_IN_PROGRESS
+		DELETE_FAILED
+		UPDATE_IN_PROGRESS
+		UPDATE_COMPLETE_CLEANUP_IN_PROGRESS
+		UPDATE_COMPLETE
+		UPDATE_ROLLBACK_IN_PROGRESS
+		UPDATE_ROLLBACK_FAILED
+		UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS
+		UPDATE_ROLLBACK_COMPLETE
+		REVIEW_IN_PROGRESS
+	)
+	_status=$(aws cloudformation describe-stacks --stack-name ${_stack_name} | jq -r '.Stacks[].StackStatus')
+
+	aws cloudformation list-stacks --stack-status-filter ${_filter[@]} | jq -r '.StackSummaries[].StackName' | grep ${_stack_name}
+	return $?
+
+
+}
+
 
 function get_output_value()
 {
-  set -eu
-  declare _stack_name
-  declare _key
-  _stack_name=$1
-  _key=$2
-  aws cloudformation describe-stacks --stack-name ${_stack_name} | jq -r '.Stacks[].Outputs[] | select(.OutputKey == "'${_key}'") | .OutputValue'
+	set -eu
+	declare _stack_name
+	declare _key
+	_stack_name=$1
+	_key=$2
+	aws cloudformation describe-stacks --stack-name ${_stack_name} | jq -r '.Stacks[].Outputs[] | select(.OutputKey == "'${_key}'") | .OutputValue'
 
 }
+
 export AWS_DEFAULT_REGION=eu-west-1
 sudo yum -y install gcc libffi-devel python-devel openssl-devel git jq
 sudo pip install -r requirements.txt --upgrade
 cd cloudformation
 python access.py > access.template
-aws cloudformation create-stack --stack-name access --template-body file://access.template --parameters  ParameterKey=KmsArn,ParameterValue=${KMS_KEY_ARN},UsePreviousValue=False --capabilities CAPABILITY_IAM
+
+stack_exists access || aws cloudformation create-stack --stack-name access --template-body file://access.template --parameters ParameterKey=KmsArn,ParameterValue=${KMS_KEY_ARN},UsePreviousValue=False --capabilities CAPABILITY_IAM
 
 wait_for_stack access
 LAMBDA_BUCKET=$(get_output_value access LambdaBucket)
@@ -58,5 +92,5 @@ aws s3 cp build/publish/bless_lambda.zip s3://${LAMBDA_BUCKET}
 make clean
 cd ../cloudformation
 python ssh-ca-api.py | aws s3 cp - s3://${CFN_BUCKET}/ssh-ca-api.template
-aws cloudformation create-stack --stack-name ssh-ca-api2 --template-url "https://s3.amazonaws.com/${CFN_BUCKET}/ssh-ca-api.template" --parameters  ParameterKey=AccessStack,ParameterValue=access,UsePreviousValue=False --capabilities CAPABILITY_IAM
+stack_exists ssh-ca-api2 || aws cloudformation create-stack --stack-name ssh-ca-api2 --template-url "https://s3.amazonaws.com/${CFN_BUCKET}/ssh-ca-api.template" --parameters ParameterKey=AccessStack,ParameterValue=access,UsePreviousValue=False --capabilities CAPABILITY_IAM
 wait_for_stack ssh-ca-api2
